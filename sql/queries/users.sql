e30824ca218aca42497299d9552925d2f76fac9e
Larcyn78 <jrasch78@gmail.com>
2026-04-05 08:38:25 -0600

    corrections for auth

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -2,3 +2,8 @@
 INSERT INTO users (id,created_at,updated_at,email,hashed_password)
 VALUES(gen_random_uuid(),now(),now(),$1,$2)
 RETURNING *;
+
+
+-- name: GetUserByEmail :one
+SELECT * FROM users
+WHERE email = $1;
